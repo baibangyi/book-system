c13beb54257dc348ab499fd78ce0b483f4bb7947
baibangyi <15808022614@163.com>
2018-02-27 11:28:49 +0800

    finish db connect

--- module/book.js
+++ module/book.js
@@ -1,8 +1,9 @@
-var mongoose = require('mongoose')
-var db = mongoose.createConnection('localhost','book')
-db.on('error',console.error.bind(console,'连接错误'))
-db.once('open',function() {
-	console.log('数据库连接成功，请继续操作')
-})
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/book');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('数据库链接成功')
+});
 
-module.exports = db
+module.exports = db;
--- module/bookSchema.js
+++ module/bookSchema.js
@@ -5,3 +5,17 @@ var bookSchema = new mongoose.Schema({
 	summary: String,
 	author: String
 })
+
+bookSchema.statics.findBook = function(obj, callback) {
+	this.find(obj,callback)
+}
+
+bookSchema.statics.updateBook = function(obj,newObj,callback) {
+	this.update(obj,newObj,callback)
+}
+
+bookSchema.statics.removeBook = function(obj,callback) {
+	this.remove(obj,callback)
+}
+
+module.exports = bookSchema;
