4d7dc95c183cbb223fa6d4c2ebe818626fb7cdfb
baibangyi <15808022614@163.com>
2018-03-03 10:52:09 +0800

    update book.js

--- module/book.js
+++ module/book.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://localhost:27017/book')
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
