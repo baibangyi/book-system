2400e31c81433d86530b57f080de9458252c551b
baibangyi <15808022614@163.com>
2018-03-04 19:43:04 +0800

    refresh key

--- module/book.js
+++ module/book.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
+mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://localhost:27017/book')
 var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', function (error) {
+  console.log("数据库连接失败："+error)
+});
 db.once('open', function() {
   console.log('数据库链接成功')
 });
 
 var bookSchema = new mongoose.Schema({
-  title:{
-    unique:true,
-    type:'String'
-  },
+  title:'String',
   summary:'String',
   price:'Number',
   meta:{
